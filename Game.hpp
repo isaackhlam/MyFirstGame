eabcc29ab81fa84e84000b5529fbb3ef77e78d77
CommitSudoku <97761927+isaackhlam@users.noreply.github.com>
2022-04-29 09:59:00 +0800

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -18,18 +18,25 @@ class Game{
         void spawnEnemy();
         void processInput();
         void updateMousePosition();
+        void updateEnemies();
         void update(sf::Time);
+        void renderEnemies();
         void render();
         void run();
 
     private:
-        //
+        // Variables
         sf::RenderWindow window;
-        sf::Texture texture;
-        sf::Sprite sprite;
+        sf::Event event;
+
+        // Mouse Position
+        sf::Vector2i mousePosWindow;
+
+        // Game objects
         std::vector<sf::RectangleShape> enemies;
         sf::RectangleShape enemy;
-        sf::Vector2i mousePosWindow;
+        sf::Texture texture;
+        sf::Sprite sprite;
 };
 
 #endif
